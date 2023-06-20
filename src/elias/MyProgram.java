5a9fe3f1ad5a5c97f131d446a9be46c10cd02c9e
elias ‎ <ebarrellet@gmail.com>
2023-06-20 19:43:22 -0400

    first commit

---
+++ src/elias/MyProgram.java
@@ -0,0 +1,72 @@
+package elias;
+
+import java.util.Scanner;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Elias Barrellet");
+		System.out.println("");
+		
+		
+		int n1 = 74;
+		int n2 = 36;
+		System.out.println(n1 + n2);
+		System.out.println("");
+		
+		
+		int n3 = 125;
+		int n4 = 24;
+		System.out.println(n3/n4);
+		System.out.println("");
+		
+		
+		System.out.println("   J    a  v     v  a");
+		System.out.println("   J   a a  v   v  a a ");
+		System.out.println("J  J  aaaaa  V V  aaaaa");
+		System.out.println(" JJ  a    a   V  a     a");
+		System.out.println("");
+		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int number3 = scanner.nextInt();
+
+        int sum = number1 + number2 + number3;
+        System.out.println("The sum of the three numbers is: " + sum);
+        System.out.println("");
+        scanner.close();
+        
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("");
+        
+        System.out.println("+\"\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
+	}
+
+}
